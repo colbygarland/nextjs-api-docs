af81c16b0f7f67dedf27712529c66921b6f058c2
Colby Garland <colbygarland@gmail.com>
2021-02-05 09:22:25 -0800

    Update the footer

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -2,7 +2,7 @@ export const PageFooter = () => (
   <footer className="py-2 bg-gray-800 text-white px-4 lg:px-8">
     <small>
       Created by
-      <a href="https://github.com/colbygarland" target="_blank">
+      <a className="inline-block ml-2 font-bold" href="https://github.com/colbygarland" target="_blank">
         Colby Garland
       </a>
       .
